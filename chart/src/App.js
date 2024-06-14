8362efacf0fbfbf71a7442ae1a149839b258177a
alessosos <bradburylilip@gmail.com>
2024-06-13 22:24:44 -0700

    charts

---
+++ chart/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import BarChart from './charts/BarChart';
+import DoughnutChart from '/charts/DoughnutChart';
+import LineChart from './charts/LineChart';
+import PieChart from './charts/PieChart';
+
+function App() {
+  return (
+    <div className="App">
+      <BarChart />
+      {/* <DoughnutChart /> */}
+      {/* <LineChart /> */}
+      {/* <PieChart /> */}
+    </div>
+  );
+}
+
+export default App;
